a9d2b79258dcfa2a321e0b64b78237901d0fa380
Moamen Abdelwahed <moomen.abdelwahed@gmail.com>
2025-05-12 17:37:19 +0300

    Update project references from Naya Dashboard to MegaPlug Dashboard and adjust environment configurations accordingly

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    // apiUrl: 'https://staging-api.nayastarter.com/api',
-    apiUrl: 'http://68.183.208.161/development/api/admin/',
+    // apiUrl: 'https://staging-api.megaplugstarter.com/api',
+    apiUrl: 'http://134.199.221.215:82/api/v1/backoffice/',
     version: '1.0.0',
     defaultLanguage: 'en',
     supportedLanguages: ['en', 'tr'],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://68.183.208.161/development/api/admin/',
-    appName: 'Naya Starter',
+    apiUrl: 'http://134.199.221.215:81/api/v1/backoffice/',
+    appName: 'MegaPlug Starter',
     version: '1.0.0',
     defaultLanguage: 'en',
     supportedLanguages: ['en', 'tr'],
