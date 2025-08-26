ba7b1722c0feec6467d256bd69062b53899248c6
ali.hefny <ali.badr.hefny@gmail.com>
2025-08-27 02:42:48 +0300

    ch

--- src/app/core/global-error-handler.ts
+++ src/app/core/global-error-handler.ts
@@ -3,9 +3,9 @@ import { ErrorHandler, Injectable } from '@angular/core';
 @Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
   private readonly excludedErrorMessages: string[] = [
-    'MatSortHeader must be placed within a parent element with the MatSort directive',
-    'ExpressionChangedAfterItHasBeenCheckedError: Expression has changed after it was checked',
-    'Cannot read properties of null (reading \'sortChange\')'
+    // 'MatSortHeader must be placed within a parent element with the MatSort directive',
+    // 'ExpressionChangedAfterItHasBeenCheckedError: Expression has changed after it was checked',
+    // 'Cannot read properties of null (reading \'sortChange\')'
   ];
 
   handleError(error: any): void {
